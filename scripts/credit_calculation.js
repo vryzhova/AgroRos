a9e2afb85dcca0b31b0613eeb6e9b35a6bfe3b68
olga-mironowa <oapnsk@mail.ru>
2022-06-15 19:14:01 +0300

    add calc credit 1

--- scripts/credit_calculation.js
+++ scripts/credit_calculation.js
@@ -41,7 +41,7 @@ function showTabsContent(b){
 
 // ползунок
 
-class Slider 
+class SliderCredit 
 {
     constructor (rangeElement, valueElement, options) 
     {
@@ -49,7 +49,7 @@ class Slider
     this.valueElement = valueElement;
     this.options = options;
       // Attach a listener to "change" event
-    this.rangeElement.addEventListener('input', this.updateSlider.bind(this))
+    this.rangeElement.addEventListener('input', this.updateSliderCredit.bind(this))
     }
 
     // Initialize the slider
@@ -58,7 +58,7 @@ class Slider
     this.rangeElement.setAttribute('min', options.min)
     this.rangeElement.setAttribute('max', options.max)
     this.rangeElement.value = options.cur
-    this.updateSlider()
+    this.updateSliderCredit()
     }
 
     // Format the money
@@ -79,10 +79,11 @@ class Slider
     }
     
 
-    updateSlider (newValue) 
+    updateSliderCredit (newValue) 
     {
     this.valueElement.innerHTML = this.asMoney(this.rangeElement.value)
     this.rangeElement.style = this.generateBackground(this.rangeElement.value)
+ 
     }
     
     
@@ -95,28 +96,27 @@ console.log(rangeElementkreditsum)
 let valueElementkreditsum = document.querySelector('.range .range__value.kredit.sum span') ;
 console.log(valueElementkreditsum)
 
+
 let optionskreditsum = 
 {
     min: 100000,
     max: 5000000,
     cur: 1000000
 };
-console.log(optionskreditsum)
+console.log(optionskreditsum);
 
 if (rangeElementkreditsum) 
 {
-    let slider = new Slider(rangeElementkreditsum, valueElementkreditsum, optionskreditsum)
-    slider.init(optionskreditsum)
-}
+    let sliderCredit = new SliderCredit(rangeElementkreditsum, valueElementkreditsum, optionskreditsum)
+    sliderCredit.init(optionskreditsum);   
+};
 
 
 // расчет кредита срок кредита
 
 let rangeElementkreditperiod = document.querySelector('.range.kredit.period [type="range"]');
-console.log(rangeElementkreditperiod )
 
 let valueElementkreditperiod  = document.querySelector('.range .range__value.kredit.period span') ;
-console.log(valueElementkreditperiod )
 
 let optionskreditperiod  = 
 {
@@ -124,15 +124,98 @@ let optionskreditperiod  =
     max: 5,
     cur: 3
 };
-console.log(optionskreditperiod )
 
 if (rangeElementkreditperiod ) 
 {
-    let slider = new Slider(rangeElementkreditperiod, valueElementkreditperiod, optionskreditperiod)
-    slider.init(optionskreditperiod);
-
-    cvalueElementkreditperiod;
+    let sliderCredit = new SliderCredit(rangeElementkreditperiod, valueElementkreditperiod, optionskreditperiod)
+    sliderCredit.init(optionskreditperiod);
 }
+
+document.getElementById('rangeKreditPayment').innerHTML = new Intl.NumberFormat('ru-RU').format(31967.55);
+
+// если меняется сумма
+
+rangeKreditSum.oninput = function () 
+{
+    let rangeKreditSum = Number(document.getElementById('rangeKreditSum').value);
+
+    let rangeKreditPeriod = Number(document.getElementById('rangeKreditPeriod').value); // срок кредита в годах
+
+    let rangeKreditPeriodMonth = Number(rangeKreditPeriod*12); // срок кредита в мес
+    
+    let rangeKreditRateMonth;
+
+    let rangeKreditPayment = 0;  // платеж ежемесячный аннуитетный
+
+    let rangeKreditRate = document.getElementById('rangeKreditRate').innerHTML;  // годовая процентная ставка
+
+    if (Number(rangeKreditSum) <= 1000000) 
+        {
+            document.getElementById('rangeKreditRate').innerHTML = '9.4%';
+            rangeKreditRateMonth = Number(9.4/ 100 / 12).toFixed(4); // месячная проц ставка        
+        }
+        else 
+        {
+            if (Number(rangeKreditSum) <= 3000000)
+                {
+                    document.getElementById('rangeKreditRate').innerHTML = '9.8%';
+                    rangeKreditRateMonth = Number(9.8 / 100 / 12).toFixed(4); // месячная проц ставка            
+                }
+            else
+                {
+                    document.getElementById('rangeKreditRate').innerHTML = '10.2%'
+                    rangeKreditRateMonth = Number(10.2 / 100 / 12).toFixed(4); // месячная проц ставка
+                }
+        }
+
+    rangeKreditPayment = Number(Number(rangeKreditSum)*((Number(rangeKreditRateMonth)*Number((Number(rangeKreditRateMonth)+1))**(Number(rangeKreditPeriodMonth)))/((Number(rangeKreditRateMonth)+1)**(Number(rangeKreditPeriodMonth))-1))).toFixed(2)
+
+    document.getElementById('rangeKreditPayment').innerHTML = new Intl.NumberFormat('ru-RU').format(rangeKreditPayment);
+}
+
+// если меняется срок
+
+rangeKreditPeriod.oninput = function () 
+{
+    let rangeKreditSum = Number(document.getElementById('rangeKreditSum').value);
+
+    let rangeKreditPeriod = Number(document.getElementById('rangeKreditPeriod').value); // срок кредита в годах
+
+    let rangeKreditPeriodMonth = Number(rangeKreditPeriod*12); // срок кредита в мес
+    
+    let rangeKreditRateMonth;
+
+    let rangeKreditPayment = 0;  // платеж ежемесячный аннуитетный
+
+  //  let rangeKreditRate = document.getElementById('rangeKreditRate').innerHTML;  // годовая процентная ставка
+
+    if (Number(rangeKreditSum) <= 1000000) 
+        {
+            document.getElementById('rangeKreditRate').innerHTML = '9.4%';
+
+            rangeKreditRateMonth = Number(9.4/ 100 / 12).toFixed(4); // месячная проц ставка        
+        }
+        else 
+        {
+            if (Number(rangeKreditSum) <= 3000000)
+                {
+                    document.getElementById('rangeKreditRate').innerHTML = '9.8%';
+
+                    rangeKreditRateMonth = Number(9.8 / 100 / 12).toFixed(4); // месячная проц ставка            
+                }
+            else
+                {
+                    document.getElementById('rangeKreditRate').innerHTML = '10.2%';
+
+                    rangeKreditRateMonth = Number(10.2 / 100 / 12).toFixed(4); // месячная проц ставка
+                }
+        }
+
+    rangeKreditPayment = Number(Number(rangeKreditSum)*((Number(rangeKreditRateMonth)*Number((Number(rangeKreditRateMonth)+1))**(Number(rangeKreditPeriodMonth)))/((Number(rangeKreditRateMonth)+1)**(Number(rangeKreditPeriodMonth))-1))).toFixed(2)
+
+    document.getElementById('rangeKreditPayment').innerHTML = new Intl.NumberFormat('ru-RU').format(rangeKreditPayment);
+}
+
 
 
 
